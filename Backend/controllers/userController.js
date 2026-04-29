d5d463e9b8b423a47b20e44bba4ffffaedd54997
madhur44-byte <katariamadhur5@gmail.com>
2026-04-29 23:34:31 +0530

    login and sign up controllers done

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,6 +1,39 @@
-export const Login = (req,res)=>{
-console.log("hello login")
+import User from "./models/userModel.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+export const   Register =  async (req,res)=>{
+try {
+  const {email,username,password} =   req.body
+
+const existuser =   await  User.findOne({email: email})
+  if (existuser ){
+return res.status(400).json({message:"User already exist"})
+  }
+
+
+const hashedpassword =  await bcrypt.hash(password,10)
+const newUser = await User.create({
+    email,
+    password:hashedpassword,
+    username
+  })
+  res.status(201).json({message:"User has been created",newUser})
 }
-export const Register = (req,res)=>{
-console.log("hello register")
+catch (error) {
+  
+res.status(500).json({message:error.message})}
+
+
+}
+export const Login = async (req,res)=>{
+const {email,password} = req.body
+const existuser = await User.findOne({email:email})
+
+if (!existuser){
+  return res.status(400).json({message:"User does not exist"})
+}
+const isMatch = await bcrypt.compare(password ,existuser.password)
+if (!isMatch){
+return  res.status(400).json({message:"incorrect password"})
+}
 }
